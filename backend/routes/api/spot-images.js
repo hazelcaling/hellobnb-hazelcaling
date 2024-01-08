a3d9ac6130712024a7490c3e30e689a87a5b086e
hazelcaling <hazelcaling@live.com>
2024-01-08 00:08:30 -0800

    fixed delete spot image by id

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -7,14 +7,13 @@ const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth')
 
 // Delete a Spot Image
 router.delete('/:imageId', requireAuth,async (req, res) => {
-    const img = await Image.findByPk(req.params.imageId);
-    const spot = await Spot.findOne({where: {ownerId: req.user.id}})
+    const img = await Image.findByPk(req.params.imageId, {include: [{ model: Spot}]});
     if (!img) return res.status(404).json({ message: "Spot Image couldn't be found"})
-    if (req.user) {
-        if (req.user.id !== spot.ownerId) return res.status(403).json({message: 'Forbidden'});
-        await Image.destroy({where: {id: req.params.imageId}});
-        res.json({ message: "Successfully deleted"})
-    }
+
+    if (req.user.id !== Spot.ownerId) return res.status(403).json({message: 'Forbidden'});
+    await Image.destroy({where: {id: req.params.imageId}});
+    res.json({ message: "Successfully deleted"})
+
 });
 
 
