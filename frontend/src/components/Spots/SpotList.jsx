09fae61157aa2eeeb43807ae0d26e2696df2ecea
hazelcaling <hazelcaling@live.com>
2024-03-24 22:18:11 -0700

    redo db

--- frontend/src/components/Spots/SpotList.jsx
+++ frontend/src/components/Spots/SpotList.jsx
@@ -1,17 +1,15 @@
 import { FaStar } from "react-icons/fa"
 import { Link } from "react-router-dom"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 import { loadSpots } from "../../store/spots"
 import "./Spotlist.css"
 
 export default function SpotList() {
-  const spots = useSelector((state) => state.spots)
+  // const spots = useSelector((state) => state.spots)
   const dispatch = useDispatch()
-  const spotList = Object.values(spots)
-  const sortedSpots = spotList.sort(
-    (a, b) => new Date(b?.createdAt) - new Date(a?.createdAt)
-  )
+  const [spots, setSpots] = useState({})
+  const [isLoaded, setIsLoaded] = useState(false)
 
   // const sortedSpotList = sortedSpots.map((spot) => (
   //   <div
@@ -49,46 +47,58 @@ export default function SpotList() {
   // ))
 
   useEffect(() => {
-    dispatch(loadSpots())
+    dispatch(loadSpots()).then((response) => setSpots(response.Spots))
+    setIsLoaded(true)
   }, [dispatch])
+  const spotList = Object.values(spots)
+  console.log("line 53", spots)
+  const sortedSpots = spotList.sort(
+    (a, b) => new Date(b?.createdAt) - new Date(a?.createdAt)
+  )
 
   // return <div className="spots-container-spotlist">{sortedSpotList}</div>
   return (
-    <div className="spots-container-spotlist">
-      {sortedSpots.map((spot) => (
-        <div
-          key={spot?.id}
-          className="spot-container-spotlist"
-          title={spot?.name}
-        >
-          <Link to={`${spot?.id}`}>
-            <div className="spot-image-container-spotlist">
-              {spot?.previewImage !== "No image" ? (
-                <img
-                  src={spot?.previewImage}
-                  style={{ maxWidth: "100%", maxHeight: "100%" }}
-                  alt={spot?.name}
-                />
-              ) : (
-                <img
-                  src="https://via.placeholder.com/300"
-                  style={{ maxWidth: "100%", maxHeight: "100%" }}
-                />
-              )}
+    <>
+      {isLoaded && (
+        <div className="spots-container-spotlist">
+          {sortedSpots.map((spot) => (
+            <div
+              key={spot?.id}
+              className="spot-container-spotlist"
+              title={spot?.name}
+            >
+              <Link to={`${spot?.id}`}>
+                <div className="spot-image-container-spotlist">
+                  {spot?.previewImage !== "No image" ? (
+                    <img
+                      src={spot?.previewImage}
+                      style={{ maxWidth: "100%", maxHeight: "100%" }}
+                      alt={spot?.name}
+                    />
+                  ) : (
+                    <img
+                      src="https://via.placeholder.com/300"
+                      style={{ maxWidth: "100%", maxHeight: "100%" }}
+                    />
+                  )}
+                </div>
+                <div className="details-spotlist">
+                  <div className="location-spotlist">
+                    {spot?.city}, {spot?.state}
+                  </div>
+                  <div className="rating-spotlist">
+                    <FaStar />{" "}
+                    {spot?.avgRating === null
+                      ? "New"
+                      : spot?.avgRating?.toFixed(1)}
+                  </div>
+                </div>
+                <div className="price-spotlist">{`$${spot?.price} night`}</div>
+              </Link>
             </div>
-            <div className="details-spotlist">
-              <div className="location-spotlist">
-                {spot?.city}, {spot?.state}
-              </div>
-              <div className="rating-spotlist">
-                <FaStar />{" "}
-                {spot?.avgRating === null ? "New" : spot?.avgRating?.toFixed(1)}
-              </div>
-            </div>
-            <div className="price-spotlist">{`$${spot?.price} night`}</div>
-          </Link>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   )
 }
